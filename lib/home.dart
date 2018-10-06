fee9cd382cb6dd5f0c5026c35000651e1690ce3d
James Liauw <jamesliauw@163.com>
2018-10-06 17:02:59 +0800

    feat: add single card

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,3 @@
-// Copyright 2018-present the Flutter authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -23,9 +9,62 @@ class HomePage extends StatelessWidget {
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
       // TODO: Add app bar (102)
+      appBar: AppBar(
+        // TODO: Add buttons and title (102)
+        leading: IconButton(
+          icon: Icon(Icons.menu, semanticLabel: 'menu'),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+
+        title: Text('SHRINE'),
+        // TODO: Add trailing buttons (102)
+        // 在Icon类中，SemanticLabel字段是在Flutter中添加辅助功能信息的常用方法
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search, semanticLabel: 'search'),
+            onPressed: () {
+              print('Search button');
+          }),
+          IconButton(
+            icon: Icon(Icons.tune, semanticLabel: 'filter'),
+            onPressed: () {
+              print('Filter button');
+          }),
+        ],
+
+      ),
       // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      body:GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(16.0),
+        childAspectRatio: 8.0/9.0,
+        // TODO: Build a grid of cards (102)
+        children: <Widget>[
+        Card(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 18.0 / 11.0,
+                child: Image.asset('assets/diamond.png'),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Title'),
+                    SizedBox(height: 8.0),
+                    Text('Secondary Text'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
       ),
     );
   }
