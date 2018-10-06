09c55470670df8d27c29815c1bb694741d10ed40
James Liauw <jamesliauw@163.com>
2018-10-06 17:15:33 +0800

    feat: add multi card

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,30 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   // TODO: Make a collection of cards (102)
+  // 封装生成任意数量的空卡，并返回一个卡列表
+  List<Card> _buildGridCards(int count) {
+    List<Card> cards = List.generate(count, 
+    (int index) => Card(child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        AspectRatio(child: Image.asset('assets/diamond.png'), aspectRatio: 18.0/11.0),
+        Padding(
+          padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Title'),
+              SizedBox(height: 8.0),
+              Text('Secondary Text')
+            ],
+        ),)
+      ]
+    ),)
+    );
+
+    return cards;
+  }
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
@@ -41,30 +65,7 @@ class HomePage extends StatelessWidget {
         padding: EdgeInsets.all(16.0),
         childAspectRatio: 8.0/9.0,
         // TODO: Build a grid of cards (102)
-        children: <Widget>[
-        Card(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              AspectRatio(
-                aspectRatio: 18.0 / 11.0,
-                child: Image.asset('assets/diamond.png'),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Title'),
-                    SizedBox(height: 8.0),
-                    Text('Secondary Text'),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        )
-      ],
+        children: this._buildGridCards(10)
       ),
     );
   }
